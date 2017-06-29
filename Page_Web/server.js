6c90a9052ce02b43551c8a2bf011530009dd57e8
Othmane FARAJALLAH <othfarajallah@hotmail.fr>
2017-06-29 16:41:44 +0200

    Ajout du traitement des requetes post

--- Page_Web/server.js
+++ Page_Web/server.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static('public'));
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
 // Permet de fixer le dossier public comme étant le dossier pour les fichiers statics
-app.use(express.static('public'));
-// index page
+
+// Page d'accueil
 app.get('/', function(req, res) {
     res.render('index.ejs');
 });
 
+// Page de parametres
 app.get('/parametres', function(req, res) {
     res.render('parametres.ejs');
+});
+app.post('/parametres', function(req, res){
+   console.log(req);
+   res.send("recieved your request!");
 });
 
 app.listen(8080);
