d6669e4be35e17f948dc5554f69234309da6fbd0
jeremie-dautheribes <jeremie.dautheribes@etu.umontpellier.fr>
2017-06-29 16:27:20 +0200

    Ajout de la page de login

--- Page_Web/server.js
+++ Page_Web/server.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var app = express();
+var firstLog = true; // Variable pour le login
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 // Permet de fixer le dossier public comme étant le dossier pour les fichiers statics
 app.use(express.static('public'));
 // index page
-app.get('/', function(req, res) {
-    res.render('index.ejs');
-});
 
-app.get('/parametres', function(req, res) {
-    res.render('parametres.ejs');
-});
+if(firstLog){
+
+    app.get('/', function(req, res) {
+        res.render('enregistrement.ejs');
+    })
+
+    app.get('/:chemin', function(req, res) {
+        res.render('enregistrement.ejs');
+    })
+}
+
+else {
+    app.get('/', function(req, res) {
+        res.render('index.ejs');
+    });
+
+    app.get('/parametres', function(req, res) {
+        res.render('parametres.ejs');
+    });
+}
 
 app.listen(8080);
 console.log('8080 is the magic port');
