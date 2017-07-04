6ba854ddc26bceeaab356f0fd97532c089bb1301
Othmane FARAJALLAH <othfarajallah@hotmail.fr>
2017-07-05 01:48:12 +0200

    fixing conflicts with jermie's commit bis

--- Page_Web/server.js
+++ Page_Web/server.js
@@ -5,19 +5,37 @@ var firstLog = true; // Variable pour le login
 app.set('view engine', 'ejs');
 // Permet de fixer le dossier public comme étant le dossier pour les fichiers statics
 
-// Page d'accueil
-app.get('/', function(req, res) {
-    res.render('index.ejs');
-});
 
-// Page de parametres
-app.get('/parametres', function(req, res) {
-    res.render('parametres.ejs');
-});
-app.post('/parametres', function(req, res){
-   console.log(req);
-   res.send("recieved your request!");
-});
+
+
+
+
+
+if(firstLog){
+  // Page d'accueil
+    app.get('/', function(req, res) {
+        res.render('enregistrement.ejs');
+    })
+
+    app.get('/:chemin', function(req, res) {
+        res.render('enregistrement.ejs');
+    })
+}
+
+else {
+    app.get('/', function(req, res) {
+        res.render('index.ejs');
+    });
+    // Page de parametres
+    app.get('/parametres', function(req, res) {
+        res.render('parametres.ejs');
+    });
+    app.post('/parametres', function(req, res){
+       console.log(req);
+       res.send("recieved your request!");
+    });
+
+}
 
 app.listen(8080);
 console.log('8080 is the magic port');
