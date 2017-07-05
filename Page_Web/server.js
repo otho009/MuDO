517172b6d06a41ed11b0a539f3accd680aab5d8d
Othmane FARAJALLAH <othfarajallah@hotmail.fr>
2017-07-05 18:14:39 +0200

    recuperation des champs de parametres

--- Page_Web/server.js
+++ Page_Web/server.js
@@ -1,41 +1,46 @@
 var express = require('express');
 var app = express();
-var firstLog = true; // Variable pour le login
+var bodyParser= require('body-parser');
 
 app.set('view engine', 'ejs');
 // Permet de fixer le dossier public comme étant le dossier pour les fichiers statics
+app.use(express.static('public'));
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 
-
-
-
-
-
-if(firstLog){
-  // Page d'accueil
+  // Page d'enregistrement
     app.get('/', function(req, res) {
         res.render('enregistrement.ejs');
-    })
+    });
 
-    app.get('/:chemin', function(req, res) {
-        res.render('enregistrement.ejs');
-    })
-}
 
-else {
-    app.get('/', function(req, res) {
+
+   // Page d'acceuil
+    app.get('/accueil', function(req, res) {
         res.render('index.ejs');
     });
     // Page de parametres
     app.get('/parametres', function(req, res) {
         res.render('parametres.ejs');
     });
+
     app.post('/parametres', function(req, res){
-       console.log(req);
+       console.log(req.body);
        res.send("recieved your request!");
     });
+    // configuration du materiel
+    app.post('/parametres2', function(req, res){
+       console.log(req.body);
+       res.send("recieved your request 2!");
+    });
+    // app.post('/', function(req, res){
+    //    console.log(req.body);
+    //    res.send("recieved your request!");
+    // });
 
-}
 
 app.listen(8080);
 console.log('8080 is the magic port');
