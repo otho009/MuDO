b90b9212aba1b05f2a896d2628282d6d69ae8889
Othmane FARAJALLAH <othfarajallah@hotmail.fr>
2017-07-07 11:24:45 +0200

    mise en place de l'authentification

--- Page_Web/server.js
+++ Page_Web/server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser= require('body-parser');
 var session = require('express-session');
-
+var verfication={'login': 'admin' , 'password': 'admin' };
+var sess;
 
 app.set('view engine', 'ejs');
 // Permet de fixer le dossier public comme étant le dossier pour les fichiers statics
@@ -18,30 +19,53 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false }
 }));
+// Authentication and Authorization Middleware
+var auth = function(req, res, next) {
+  if (sess && sess.logedIn )
+    return next();
+  else
+    return res.sendStatus(401);
+};
 
   // Page d'enregistrement
     app.get('/', function(req, res) {
+      sess= req.session;
         res.render('enregistrement.ejs');
     });
    // Page d'acceuil
-    app.get('/accueil', function(req, res) {
+    app.get('/accueil', auth, function(req, res) {
         res.render('index.ejs');
     });
     // Page de parametres
-    app.get('/parametres', function(req, res) {
+    app.get('/parametres', auth, function(req, res) {
         res.render('parametres.ejs');
     });
 
 
     // personnalisation
-    app.post('/parametres', function(req, res){
+    app.post('/parametres',auth,function(req, res){
        console.log(req.session);
     });
     // configuration du matériel
-    app.post('/parametres2', function(req, res){
+    app.post('/parametres2',auth,function(req, res){
        console.log(req.body);
        res.send("recieved your request 2!");
     });
+    app.post('/', function(req, res){
+      sess.email= req.body.email;
+      sess.password= req.body.password;
+      console.log(sess.email);
+
+      if (sess.email== verfication.login && sess.password == verfication.password){
+         sess.logedIn=true;
+        res.redirect('/accueil');
+      }
+      else{
+        res.redirect('/');
+      }
+
+    });
+
 
 
 
